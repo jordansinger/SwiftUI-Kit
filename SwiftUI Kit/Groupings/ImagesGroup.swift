8c3a7714ca1560b2d3ef1c48a98fbec8cdfbb184
Jordan Singer <singer.jordan@gmail.com>
2020-07-10 19:29:45 -0400

    Image examples

--- SwiftUI Kit/Groupings/ImagesGroup.swift
+++ SwiftUI Kit/Groupings/ImagesGroup.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct ImagesGroup: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SectionView(
+            title: "Image",
+            description: "A view that displays an environment-dependent image.",
+            content: {
+                Image("Image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 128)
+            }
+        )
+        
+        SectionView(
+            title: "Label",
+            description: "A standard label for user interface items, consisting of an icon with a title.",
+            content: {
+                Group {
+                    Label("Rain", systemImage: "cloud.rain")
+                    Label("Snow", systemImage: "snow")
+                    Label("Sun", systemImage: "sun.max")
+                }
+            }
+        )
     }
 }
 

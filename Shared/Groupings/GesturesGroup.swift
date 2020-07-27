ed1232fe9d5b28eb2f1b32568c4d94b8cd3fd3ea
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-27 09:26:06 +0530

    GesturesGroup added

---
+++ Shared/Groupings/GesturesGroup.swift
@@ -0,0 +1,20 @@
+//
+//  GesturesGroup.swift
+//  SwiftUI Kit iOS
+//
+//  Created by Aaryan Kothari on 27/07/20.
+//
+
+import SwiftUI
+
+struct GesturesGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GesturesGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        GesturesGroup()
+    }
+}

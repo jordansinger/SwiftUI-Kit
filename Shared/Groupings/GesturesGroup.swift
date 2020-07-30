4f4dad4b7ddb444d4f9c633590ed3fd4027ed487
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-07-30 10:49:13 +0530

    Add Gestures (#39)
    Gestures included:
    -  Tap Gesture
    -  Drag Gesture
    -  LongPress Gesture

---
+++ Shared/Groupings/GesturesGroup.swift
@@ -0,0 +1,106 @@
+//
+//  GesturesGroup.swift
+//  SwiftUI Kit iOS
+//
+//  Created by Aaryan Kothari on 27/07/20.
+//
+
+import SwiftUI
+
+struct GesturesGroup: View {
+    var body: some View {
+        Group{
+            SectionView(description: "A Gesture that counts the number of taps."){
+                TapGestureBlock()
+            }
+            
+            SectionView(description: "A Gesture that detects drag motion."){
+                DragGestureBlock()
+            }
+            
+            SectionView(description: "A Gesture that detects a Long Press."){
+                LongPressGestureBlock()
+            }
+        }
+    }
+}
+
+struct TapGestureBlock : View {
+    @State var count = 1
+    @State var text = "Tap Count : 0"
+    var body : some View {
+        Group{
+            HStack{
+            Text("Tap Gesture")
+                Spacer()
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.secondary)
+            }
+        }
+        .onTapGesture(count:count,perform:tapped)
+    }
+    func tapped(){
+        self.text = "Tap count : \(count)"
+        self.count += 1
+    }
+}
+
+struct DragGestureBlock : View {
+    @State var color : Color = .accentColor
+    
+    var body : some View {
+        Text("Drag Gesture")
+            .gesture(drag)
+            .foregroundColor(color)
+    }
+    
+    var drag: some Gesture {
+        DragGesture(minimumDistance: 10, coordinateSpace: .local)
+            .onChanged { value in
+                self.color = (value.distance > 300) ? .green : .red
+            }
+            .onEnded { value in
+                self.color = (value.distance > 300) ? .green : .accentColor
+            }
+    }
+}
+
+struct LongPressGestureBlock: View {
+    @GestureState var isDetectingLongPress = false
+    @State var completedLongPress = false
+    
+    var longPress: some Gesture {
+        LongPressGesture(minimumDuration: 3)
+            .updating($isDetectingLongPress) { currentstate, gestureState,transaction in
+                gestureState = currentstate
+                transaction.animation = Animation.easeIn(duration: 2.0)
+            }
+            .onEnded { finished in
+                self.completedLongPress = finished
+            }
+    }
+    
+    var body: some View {
+        Text("LongPress Gesture")
+            .foregroundColor(textColor())
+            .gesture(longPress)
+    }
+    
+    func textColor()->Color{
+        return self.isDetectingLongPress ? Color.red :
+            (self.completedLongPress ? .green : .accentColor)
+    }
+}
+
+struct GesturesGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        GesturesGroup()
+    }
+}
+
+extension DragGesture.Value {
+    var distance: CGFloat {
+        return sqrt(pow(self.predictedEndLocation.x,2) + pow(self.predictedEndLocation.y,2))
+    }
+}

3816b814cf0120f5afce988c442289e3a4b62c5e
Thomas braun <tbraun1551@gmail.com>
2020-08-04 23:39:37 -0500

    Added Button Modifiers Sheet
    Added ability to view sheet in button section that displays all the modifiers on a button and their impact on the buttons when they are changed.

--- Shared/Groupings/ButtonsGroup.swift
+++ Shared/Groupings/ButtonsGroup.swift
@@ -10,6 +10,7 @@ struct ButtonsGroup: View {
     @State private var showingAlert = false
     @State private var showingSheet = false
     @State private var showingActionSheet = false
+	@State private var showButtonSheet = false
     
     var body: some View {
         Group {
@@ -147,6 +148,26 @@ struct ButtonsGroup: View {
                     )
                 }
             )
+			
+			SectionView(
+				title: "CustomButtonViews",
+				description: "Customize what buttons look like",
+				content: {
+					Button(action: {
+						self.showButtonSheet = true
+					}) {
+						Text("Show Button Modifiers")
+					}.sheet(isPresented: $showButtonSheet) {
+						ButtonModifiers().padding()
+						#if os(macOS)
+						Button("Close") {
+							showButtonSheet.toggle()
+						}.padding()
+						#endif
+					}
+				}
+		
+				)
             #endif
         }
     }

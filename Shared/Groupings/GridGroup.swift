daa02b55c9cfacd82f8be3543df375689463c18e
Thomas braun <tbraun1551@gmail.com>
2020-08-12 08:47:06 -0500

    Added widgets as well as fixed crashing on mac

---
+++ Shared/Groupings/GridGroup.swift
@@ -0,0 +1,20 @@
+//
+//  GridGroup.swift
+//  SwiftUI Kit iOS
+//
+//  Created by Thomas Braun on 8/7/20.
+//
+
+import SwiftUI
+
+struct GridGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        GridGroup()
+    }
+}
